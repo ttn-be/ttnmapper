5d2f4130e1d1089693fdb68f4ff7b6622444d9a3
Pascal Mainini <pascal@mainini.ch>
2017-05-20 12:42:21 +0200

    full refactoring

--- nmea.py
+++ nmea.py
@@ -16,105 +16,82 @@
 
 import utime
 
-class NmeaParser(object):
-    """NMEA Sentence Parser. Creates object that stores all relevant GPS data and statistics.
-    Parses sentences using update(). """
-    
+class NmeaParser:
+    """NMEA sentence parser. update() parses a sentence and stores relevant
+    GPS data and statistics as attributes."""
+
     def __init__(self):
-        """Setup GPS Object Status Flags, Internal Data Registers, etc"""
+        """Initializes attributes to useful default values."""
 
-        #####################
-        # Data From Sentences
-        # Time
-        self.utc = (0)
-        
-        # Object Status Flags
-        self.fix_time = 0
-        self.valid_sentence = False
-
-        # Position/Motion
-        self.latitude = 0.0
-        self.longitude = 0.0
-        self.altitude = 0.0
-
-        # GPS Info
-        self.satellites_in_use = 0
-        self.hdop = 0.0
-        self.fix_stat = 0
-        
-        #raw data segments
+        # Raw data segments
         self.nmea_segments = []
 
+        # General data
+        self.timestamp = ()
+        self.fix_status = 0
+        self.fix_time = 0
+        self.satellites_in_use = 0
+        self.hdop = 0.0
+
+         # Position data
+        self.altitude = 0.0
+        self.latitude = 0.0
+        self.longitude = 0.0
+
+    def __str__(self):
+        """Return meaningful string representation"""
+
+        return 'T: {}:{}:{}, Lat: {}, Lon: {}, Alt: {}, Fix: {}, Sat: {}, HDOP: {}'.format(
+            self.timestamp[0], self.timestamp[1], self.timestamp[2], self.latitude, 
+            self.longitude, self.altitude, self.fix_status, self.satellites_in_use, self.hdop)
+
     def update(self,  sentence):
-        self.valid_sentence = False
+        """Parses a given sentence and updates attributes. Returns True on
+        success, False otherwise."""
+
         self.nmea_segments = str(sentence).split(',')
-       
-        #Parse GPGGA
-        if (self.nmea_segments[0] == "b'$GPGGA"):
-            self.valid_sentence = True
-            try:
-                # UTC Timestamp
-                 utc_string = self.nmea_segments[1]
-                 
-                # Skip timestamp if receiver doesn't have on yet
-                 if utc_string:
-                    hours = int(utc_string[0:2])
-                    minutes = int(utc_string[2:4])
-                    seconds = float(utc_string[4:])
-                 else:
-                    hours = 0
-                    minutes = 0
-                    seconds = 0.0
-                    
-                 # Number of Satellites in Use
-                 satellites_in_use = int(self.nmea_segments[7])
 
-                 # Horizontal Dilution of Precision
-                 hdop = float(self.nmea_segments[8])
+        if len(self.nmea_segments) < 10:
+            # Incomplete sentence
+            return False
 
-                 # Get Fix Status
-                 fix_stat = int(self.nmea_segments[6])
-            except (ValueError, IndexError):
-                return False
-            
-         # Process Location and Speed Data if Fix is GOOD
-            if fix_stat:
-                # Longitude / Latitude
-                try:
-                    # Latitude
-                    l_string = self.nmea_segments[2]
-                    lat_degs = float(l_string[0:2])
-                    lat_mins = float(l_string[2:])
+        if self.nmea_segments[0] != "b'$GPGGA":
+            # Only GPGGA sentences are supported.
+            return False
 
-                    # Longitude
-                    l_string = self.nmea_segments[4]
-                    lon_degs = float(l_string[0:3])
-                    lon_mins = float(l_string[3:])
-                except ValueError:
-                    return False
-        
-                # Altitude / Height Above Geoid
-                try:
-                    altitude = float(self.nmea_segments[9])
-                    geoid_height = float(self.nmea_segments[11])
-                except ValueError:
-                    return False
-                    
-                # Update Object Data
-                self.latitude = lat_degs + (lat_mins/60)
-                self.longitude = lon_degs + (lon_mins/60)
-                self.altitude = altitude
-                self.geoid_height = geoid_height
-                
-            # Update Object Data
-            self.timestamp = (hours, minutes, seconds)
-            self.satellites_in_use = satellites_in_use
-            self.hdop = hdop
-            self.fix_stat = fix_stat
-        
-            # If Fix is GOOD, update fix timestamp
-            if fix_stat:
-                self.fix_time = utime.time()
+        if len(self.nmea_segments[1]) == 0:
+            # Time not yet synchronized
+            return False
 
-            return True
-            
+        # UTC Timestamp
+        utc_string = self.nmea_segments[1]
+        self.timestamp = ( int(utc_string[0:2]), int(utc_string[2:4]), float(utc_string[4:]) )
+
+        # Other data
+        self.fix_status = int(self.nmea_segments[6])
+        self.satellites_in_use = int(self.nmea_segments[7])
+        self.hdop = float(self.nmea_segments[8])        # Horizontal Dilution of Precision
+
+        if self.fix_status == 0:
+            # Don't process position if no fix has been obtained
+            return False
+
+        self.fix_time = utime.time()
+
+        # Altitude
+        self.altitude = float(self.nmea_segments[9])
+
+        # Latitude
+        l = self.nmea_segments[2]
+        lat_degs = float(l[0:2])
+        lat_mins = float(l[2:])
+        self.latitude = lat_degs + (lat_mins/60)
+
+        # Longitude
+        l = self.nmea_segments[4]
+        lon_degs = float(l[0:3])
+        lon_mins = float(l[3:])
+        self.longitude = lon_degs + (lon_mins/60)
+
+        return True
+
